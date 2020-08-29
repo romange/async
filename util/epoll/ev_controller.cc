7a4a451633abbf25758b8e2598b7506fde53f459
Roman Gershman <romange@gmail.com>
2020-08-29 14:05:50 +0300

    Introduce ev_controller using the epoll interface

---
+++ util/epoll/ev_controller.cc
@@ -0,0 +1,332 @@
+// Copyright 2020, Beeri 15.  All rights reserved.
+// Author: Roman Gershman (romange@gmail.com)
+//
+
+#include "util/epoll/ev_controller.h"
+
+#include <string.h>
+#include <signal.h>
+#include <sys/eventfd.h>
+#include <sys/epoll.h>
+
+#include <boost/fiber/operations.hpp>
+#include <boost/fiber/scheduler.hpp>
+
+#include "absl/base/attributes.h"
+#include "absl/time/clock.h"
+#include "base/logging.h"
+#include "base/proc_util.h"
+
+
+#define EV_CHECK(x)                                                           \
+  do {                                                                           \
+    int __res_val = (x);                                                         \
+    if (ABSL_PREDICT_FALSE(__res_val < 0)) {                                     \
+      char buf[128];                                                             \
+      char* str = strerror_r(-__res_val, buf, sizeof(buf));                      \
+      LOG(FATAL) << "Error " << (-__res_val) << " evaluating '" #x "': " << str; \
+    }                                                                            \
+  } while (false)
+
+
+using namespace boost;
+namespace ctx = boost::context;
+
+namespace util {
+namespace epoll {
+
+namespace {
+
+
+struct signal_state {
+  struct Item {
+    EvController* ev_controller = nullptr;
+    std::function<void(int)> cb;
+  };
+
+  Item signal_map[NSIG];
+};
+
+signal_state* get_signal_state() {
+  static signal_state state;
+
+  return &state;
+}
+
+void SigAction(int signal, siginfo_t*, void*) {
+  signal_state* state = get_signal_state();
+  DCHECK_LT(signal, _NSIG);
+
+  auto& item = state->signal_map[signal];
+  auto cb = [signal, &item] { item.cb(signal); };
+
+  if (item.ev_controller && item.cb) {
+    item.ev_controller->AsyncFiber(std::move(cb));
+  } else {
+    LOG(ERROR) << "Tangling signal handler " << signal;
+  }
+}
+
+inline uint64_t GetClockNanos() {
+  timespec ts;
+  clock_gettime(CLOCK_MONOTONIC, &ts);
+  return ts.tv_sec * std::nano::den + ts.tv_nsec;
+}
+
+
+constexpr uint64_t kIgnoreIndex = 0;
+constexpr uint64_t kNopIndex = 2;
+constexpr uint64_t kUserDataCbIndex = 1024;
+constexpr uint32_t kSpinLimit = 100;
+
+}  // namespace
+
+thread_local EvController::TLInfo EvController::tl_info_;
+
+EvController::EvController() : task_queue_(128) {
+  event_fd_ = eventfd(0, EFD_CLOEXEC | EFD_NONBLOCK);
+  CHECK_GE(event_fd_, 0);
+  epoll_fd_ = epoll_create1(EPOLL_CLOEXEC);
+  CHECK_GE(epoll_fd_, 0);
+  VLOG(1) << "Created event_fd " << event_fd_ << ", epoll_fd_ " << epoll_fd_;
+
+  volatile ctx::fiber dummy;  // For some weird reason I need this to pull
+                              // boost::context into linkage.
+}
+
+EvController::~EvController() {
+  CHECK(is_stopped_);
+  close(event_fd_);
+
+  close(epoll_fd_);
+
+  signal_state* ss = get_signal_state();
+  for (size_t i = 0; i < ABSL_ARRAYSIZE(ss->signal_map); ++i) {
+    if (ss->signal_map[i].ev_controller == this) {
+      ss->signal_map[i].ev_controller = nullptr;
+      ss->signal_map[i].cb = nullptr;
+    }
+  }
+}
+
+void EvController::Stop() {
+  AsyncBrief([this] { is_stopped_ = true; });
+  VLOG(1) << "EvController::StopFinish";
+}
+
+void EvController::Run() {
+  VLOG(1) << "EvController::Run";
+  Init();
+
+  main_loop_ctx_ = fibers::context::active();
+  fibers::scheduler* sched = main_loop_ctx_->get_scheduler();
+
+  //UringFiberAlgo* scheduler = new UringFiberAlgo(this);
+  //sched->set_algo(scheduler);
+  //this_fiber::properties<UringFiberProps>().set_name("ioloop");
+
+  is_stopped_ = false;
+
+  constexpr size_t kBatchSize = 64;
+  struct epoll_event cevents[kBatchSize];
+
+  uint32_t tq_seq = 0;
+  uint32_t num_stalls = 0;
+  uint32_t spin_loops = 0, num_task_runs = 0;
+  Tasklet task;
+
+  while (true) {
+    num_task_runs = 0;
+
+    uint64_t task_start = 0;
+
+    tq_seq = tq_seq_.load(std::memory_order_acquire);
+
+    // This should handle wait-free and "submit-free" short CPU tasks enqued using Async/Await
+    // calls. We allocate the quota of 500K nsec (500usec) of CPU time per iteration.
+    while (task_queue_.try_dequeue(task)) {
+      ++num_task_runs;
+      tl_info_.monotonic_time = GetClockNanos();
+      task();
+      if (task_start == 0) {
+        task_start = tl_info_.monotonic_time;
+      } else if (task_start + 500000 < tl_info_.monotonic_time) {
+        break;
+      }
+    }
+
+    if (num_task_runs) {
+      task_queue_avail_.notifyAll();
+    }
+
+    int timeout = 0;  // By default we do not block on epoll_wait.
+
+    if (spin_loops >= kSpinLimit) {
+      spin_loops = 0;
+
+      if (tq_seq_.compare_exchange_weak(tq_seq, WAIT_SECTION_STATE, std::memory_order_acquire)) {
+        // We check stop condition when all the pending events were processed.
+        // It's up to the app-user to make sure that the incoming flow of events is stopped before
+        // stopping EvController.
+        if (is_stopped_)
+          break;
+        ++num_stalls;
+        timeout = -1;  // We gonna block on epoll_wait.
+      }
+    }
+
+    int epoll_res = epoll_wait(epoll_fd_, cevents, kBatchSize, timeout);
+    if (epoll_res < 0) {
+      LOG(FATAL) << "TBD: " << errno << " " << strerror(errno);
+    }
+    uint32_t cqe_count = epoll_res;
+    if (cqe_count) {
+      DVLOG(2) << "Fetched " << cqe_count << " cqes";
+      DispatchCompletions(cevents, cqe_count);
+    }
+
+    if (timeout == -1) {
+      // Reset all except the LSB bit that signals that we need to switch to dispatch fiber.
+      tq_seq_.fetch_and(1, std::memory_order_release);
+    }
+
+    if (tq_seq & 1) {  // We allow dispatch fiber to run.
+      tq_seq_.fetch_and(~1U, std::memory_order_relaxed);
+      this_fiber::yield();
+    }
+
+    if (sched->has_ready_fibers()) {
+      // Suspend this fiber until others finish runnning and get blocked.
+      // Eventually UringFiberAlgo will resume back this fiber in suspend_until
+      // function.
+      DVLOG(2) << "Suspend ioloop";
+      tl_info_.monotonic_time = GetClockNanos();
+      sched->suspend();
+
+      DVLOG(2) << "Resume ioloop";
+      spin_loops = 0;
+    }
+  }
+
+  VLOG(1) << "wakeups/stalls: " << tq_wakeups_.load() << "/" << num_stalls;
+
+  VLOG(1) << "centries size: " << centries_.size();
+  centries_.clear();
+}
+
+void EvController::Arm(CbType cb, int fd, uint32_t event_mask) {
+  epoll_event ev;
+  ev.events = event_mask;
+  if (cb) {
+    if (next_free_ce_ < 0) {
+      RegrowCentries();
+      CHECK_GT(next_free_ce_, 0);
+    }
+
+    ev.data.u32 = next_free_ce_ + kUserDataCbIndex;
+    DCHECK_LT(unsigned(next_free_ce_), centries_.size());
+
+    auto& e = centries_[next_free_ce_];
+    DCHECK(!e.cb);  // cb is undefined.
+    DVLOG(1) << "GetSubmitEntry: index: " << next_free_ce_;
+
+    next_free_ce_ = e.index;
+    e.cb = std::move(cb);
+    e.index = -1;
+  } else {
+    ev.data.u32 = kIgnoreIndex;
+  }
+  CHECK_EQ(0, epoll_ctl(epoll_fd_, EPOLL_CTL_ADD, fd, &ev));
+}
+
+void EvController::Init() {
+  CHECK_EQ(0U, thread_id_) << "Init was already called";
+
+  centries_.resize(512);  // .index = -1
+  next_free_ce_ = 0;
+  for (size_t i = 0; i < centries_.size() - 1; ++i) {
+    centries_[i].index = i + 1;
+  }
+
+  thread_id_ = pthread_self();
+  tl_info_.is_ev_thread = true;
+
+  Arm(nullptr, event_fd_, EPOLLIN);
+}
+
+void EvController::DoWake() {
+  DVLOG(1) << "Wake ring " << tq_seq_.load(std::memory_order_relaxed);
+
+  tq_wakeups_.fetch_add(1, std::memory_order_relaxed);
+
+  uint64_t val = 1;
+  CHECK_EQ(8, write(event_fd_, &val, sizeof(uint64_t)));
+}
+
+void EvController::DispatchCompletions(epoll_event* cevents, unsigned count) {
+  for (unsigned i = 0; i < count; ++i) {
+    const auto& cqe = cevents[i];
+
+    // I allocate range of 1024 reserved values for the internal EvController use.
+    uint32_t user_data = cqe.data.u32;
+    if (cqe.data.u32 >= kUserDataCbIndex) {  // our heap range surely starts higher than 1k.
+      size_t index = user_data - kUserDataCbIndex;
+      DCHECK_LT(index, centries_.size());
+      const auto& item = centries_[index];
+      DCHECK(item.cb) << index;
+
+
+      item.cb(cqe.events, this);
+      continue;
+    }
+
+    if (user_data == kIgnoreIndex || kNopIndex)
+      continue;
+
+    LOG(ERROR) << "Unrecognized user_data " << user_data;
+  }
+}
+
+void EvController::RegisterSignal(std::initializer_list<uint16_t> l, std::function<void(int)> cb) {
+  auto* state = get_signal_state();
+
+  struct sigaction sa;
+  memset(&sa, 0, sizeof(sa));
+
+  if (cb) {
+    sa.sa_flags = SA_SIGINFO;
+    sa.sa_sigaction = &SigAction;
+
+    for (uint16_t val : l) {
+      CHECK(!state->signal_map[val].cb) << "Signal " << val << " was already registered";
+      state->signal_map[val].cb = cb;
+      state->signal_map[val].ev_controller = this;
+
+      CHECK_EQ(0, sigaction(val, &sa, NULL));
+    }
+  } else {
+    sa.sa_handler = SIG_DFL;
+
+    for (uint16_t val : l) {
+      CHECK(state->signal_map[val].cb) << "Signal " << val << " was already registered";
+      state->signal_map[val].cb = nullptr;
+      state->signal_map[val].ev_controller = nullptr;
+
+      CHECK_EQ(0, sigaction(val, &sa, NULL));
+    }
+  }
+}
+
+void EvController::RegrowCentries() {
+  size_t prev = centries_.size();
+  VLOG(1) << "RegrowCentries from " << prev << " to " << prev * 2;
+
+  centries_.resize(prev * 2);  // grow by 2.
+  next_free_ce_ = prev;
+  for (; prev < centries_.size() - 1; ++prev)
+    centries_[prev].index = prev + 1;
+}
+
+
+}  // namespace uring
+}  // namespace util
---
+++ util/epoll/ev_controller.h
@@ -0,0 +1,270 @@
+// Copyright 2020, Beeri 15.  All rights reserved.
+// Author: Roman Gershman (romange@gmail.com)
+//
+
+#pragma once
+
+#include <pthread.h>
+
+#include <boost/fiber/fiber.hpp>
+#include <functional>
+
+#pragma GCC diagnostic push
+#pragma GCC diagnostic ignored "-Waddress"
+#include "base/function2.hpp"
+#pragma GCC diagnostic pop
+
+#include "base/mpmc_bounded_queue.h"
+#include "util/fibers/event_count.h"
+#include "util/fibers/fibers_ext.h"
+
+struct epoll_event;
+
+namespace util {
+namespace epoll {
+
+class EvControllerPool;
+
+class EvController {
+  EvController(const EvController&) = delete;
+  void operator=(const EvController&) = delete;
+
+ public:
+  EvController();
+  ~EvController();
+
+  // Runs the poll-loop. Stalls the calling thread which will become the "EvController" thread.
+  void Run();
+
+  //! Signals EvController to stop. Does not wait for it.
+  void Stop();
+
+  using IoResult = int;
+
+
+  // event_mask passed from epoll_event.events.
+  using CbType = std::function<void(uint32_t event_mask, EvController*)>;
+
+  /**
+   * @brief Returns true if the called is running in this EvController thread.
+   *
+   * @return true
+   * @return false
+   */
+  bool InMyThread() const {
+    return pthread_self() == thread_id_;
+  }
+
+  auto thread_id() const {
+    return thread_id_;
+  }
+
+  static bool IsEvControllerThread() {
+    return tl_info_.is_ev_thread;
+  }
+
+  // Returns an approximate (cached) time with nano-sec granularity.
+  // The caller must run in the same thread as the EvController.
+  static uint64_t GetMonotonicTimeNs() {
+    return tl_info_.monotonic_time;
+  }
+
+  // Returns an 0 <= index < N, where N is the number of EvController threads in the pool of called
+  // from EvController thread. Returns -1 if called from some other thread.
+  static int32_t GetIndex() {
+    return tl_info_.ev_index;
+  }
+
+  // Internal, used by EvControllerPool
+  static void SetIndex(uint32_t index) {
+    tl_info_.ev_index = index;
+  }
+
+
+  void Arm(CbType cb, int fd, uint32_t event_mask);
+  
+  /**
+   *  Message passing functions.
+   * */
+
+  //! Fire and forget - does not wait for the function to be called.
+  //! `f` should not block, lock on mutexes or Await.
+  //! Might block the calling fiber if the queue is full.
+  template <typename Func> void AsyncBrief(Func&& brief);
+
+  //! Similarly to AsyncBrief but waits 'f' to return.
+  template <typename Func> auto AwaitBrief(Func&& brief) -> decltype(brief());
+
+  //! Similarly to AsyncBrief but 'f' but wraps 'f' in fiber.
+  //! f is allowed to fiber-block or await.
+  template <typename Func, typename... Args> void AsyncFiber(Func&& f, Args&&... args) {
+    // Ideally we want to forward args into lambda but it's too complicated before C++20.
+    // So I just copy them into capture.
+    // We forward captured variables so we need lambda to be mutable.
+    AsyncBrief([f = std::forward<Func>(f), args...]() mutable {
+      ::boost::fibers::fiber(std::forward<Func>(f), std::forward<Args>(args)...).detach();
+    });
+  }
+
+  // Please note that this function uses Await, therefore can not be used inside
+  // EvController main fiber (i.e. Async callbacks).
+  template <typename... Args> boost::fibers::fiber LaunchFiber(Args&&... args) {
+    ::boost::fibers::fiber fb;
+
+    // It's safe to use & capture since we await before returning.
+    AwaitBrief([&] { fb = boost::fibers::fiber(std::forward<Args>(args)...); });
+    return fb;
+  }
+
+  // Runs possibly awating function 'f' safely in EvController thread and waits for it to finish,
+  // If we are in his thread already, runs 'f' directly, otherwise
+  // runs it wrapped in a fiber. Should be used instead of 'AwaitBrief' when 'f' itself
+  // awaits on something.
+  // To summarize: 'f' may not block its thread, but allowed to block its fiber.
+  template <typename Func> auto AwaitBlocking(Func&& f) -> decltype(f());
+
+  void RegisterSignal(std::initializer_list<uint16_t> l, std::function<void(int)> cb);
+
+  void ClearSignal(std::initializer_list<uint16_t> l) {
+    RegisterSignal(l, nullptr);
+  }
+
+  int ev_loop_fd() const {
+    return epoll_fd_;
+  }
+
+ private:
+  enum { WAIT_SECTION_STATE = 1UL << 31 };
+
+  void Init();
+
+  void DispatchCompletions(struct epoll_event* cevents, unsigned count);
+
+  // Called only from external threads.
+  void DoWake();
+
+  void WakeupIfNeeded() {
+    auto current = tq_seq_.fetch_add(2, std::memory_order_relaxed);
+    if (current == WAIT_SECTION_STATE) {
+      // We protect DoWake using tq_seq_. That means only one thread at a time
+      // can enter here. Moreover tq_seq_ == WAIT_SECTION_STATE only when
+      // EvController enters WAIT section, therefore we do not race over SQE ring with EvController thread.
+      DoWake();
+    }
+  }
+
+  template <typename Func> bool EmplaceTaskQueue(Func&& f) {
+    if (task_queue_.try_enqueue(std::forward<Func>(f))) {
+      WakeupIfNeeded();
+
+      return true;
+    }
+    return false;
+  }
+
+  void RegrowCentries();
+  void ArmWakeupEvent();
+
+  pthread_t thread_id_ = 0U;
+
+  int event_fd_ = -1, epoll_fd_ = -1;
+  bool is_stopped_ = true;
+
+  // We use fu2 function to allow moveable semantics.
+  using Tasklet =
+      fu2::function_base<true /*owns*/, false /*non-copyable*/, fu2::capacity_default,
+                         false /* non-throwing*/, false /* strong exceptions guarantees*/, void()>;
+  static_assert(sizeof(Tasklet) == 32, "");
+
+  using FuncQ = base::mpmc_bounded_queue<Tasklet>;
+
+  using EventCount = fibers_ext::EventCount;
+
+  FuncQ task_queue_;
+  std::atomic_uint32_t tq_seq_{0}, tq_wakeups_{0};
+  EventCount task_queue_avail_;
+  ::boost::fibers::context* main_loop_ctx_ = nullptr;
+
+  friend class UringFiberAlgo;
+
+  struct CompletionEntry {
+    CbType cb;
+
+    // serves for linked list management when unused. Also can store an additional payload
+    // field when in flight.
+    int32_t index = -1;
+    int32_t unused = -1;
+  };
+  static_assert(sizeof(CompletionEntry) == 40, "");
+
+  std::vector<CompletionEntry> centries_;
+  int32_t next_free_ce_ = -1;
+
+  struct TLInfo {
+    bool is_ev_thread = false;
+    uint32_t ev_index = 0;
+    uint64_t monotonic_time = 0;
+  };
+  static thread_local TLInfo tl_info_;
+};
+
+
+// Implementation
+// **********************************************************************
+//
+template <typename Func> void EvController::AsyncBrief(Func&& f) {
+  if (EmplaceTaskQueue(std::forward<Func>(f)))
+    return;
+
+  while (true) {
+    EventCount::Key key = task_queue_avail_.prepareWait();
+
+    if (EmplaceTaskQueue(std::forward<Func>(f))) {
+      break;
+    }
+    task_queue_avail_.wait(key.epoch());
+  }
+}
+
+template <typename Func> auto EvController::AwaitBrief(Func&& f) -> decltype(f()) {
+  if (InMyThread()) {
+    return f();
+  }
+  if (IsEvControllerThread()) {
+    // TODO:
+  }
+
+  fibers_ext::Done done;
+  using ResultType = decltype(f());
+  detail::ResultMover<ResultType> mover;
+
+  // Store done-ptr by value to increase the refcount while lambda is running.
+  AsyncBrief([&mover, f = std::forward<Func>(f), done]() mutable {
+    mover.Apply(f);
+    done.Notify();
+  });
+
+  done.Wait();
+  return std::move(mover).get();
+}
+
+// Runs possibly awating function 'f' safely in ContextThread and waits for it to finish,
+// If we are in the context thread already, runs 'f' directly, otherwise
+// runs it wrapped in a fiber. Should be used instead of 'Await' when 'f' itself
+// awaits on something.
+// To summarize: 'f' should not block its thread, but allowed to block its fiber.
+template <typename Func> auto EvController::AwaitBlocking(Func&& f) -> decltype(f()) {
+  if (InMyThread()) {
+    return f();
+  }
+
+  using ResultType = decltype(f());
+  detail::ResultMover<ResultType> mover;
+  auto fb = LaunchFiber([&] { mover.Apply(std::forward<Func>(f)); });
+  fb.join();
+
+  return std::move(mover).get();
+}
+
+}  // namespace epoll
+}  // namespace util
